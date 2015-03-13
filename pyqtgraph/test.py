d40c180cf2fd440ba3124431764e1692086504bf
Nylok <santbernardo@gmail.com>
2015-03-13 04:21:49 +0000

    Update test.py

--- pyqtgraph/test.py
+++ pyqtgraph/test.py
@@ -55,6 +55,15 @@ for x in range(0,25):
 	daata[x,y,z,color] = np.random.power(2.0)*255
 
 
+#Adds new blending function to pyqtgraph/opengl/GLGraphicsItem.py
+gl.GLGraphicsItem.GLOptions['costume'] = {
+        GL_DEPTH_TEST: False,
+        GL_BLEND: True,
+        GL_ALPHA_TEST: False,
+        GL_CULL_FACE: False,
+        'glBlendFunc':(GL_CONSTANT_COLOR,GL_ONE_MINUS_CONSTANT_COLOR),
+}
+
 class GLViewWidget(gl.GLViewWidget):
 
     clicked = QtCore.pyqtSignal()
@@ -71,6 +80,10 @@ class GLViewWidget(gl.GLViewWidget):
         self.lastPos = QtCore.QPoint()
 
         self.blend_v = 0
+	self.blend_r = 0.1
+	self.blend_g = 0.1
+	self.blend_b = 0.1
+	self.blend_a = 0.1
 	
         self.opts['distance']=35
         self.opts['center']= QtGui.QVector3D(12.5,12.5,12.5)
@@ -146,6 +159,9 @@ class GLViewWidget(gl.GLViewWidget):
 		if self.blend_v == 0:
 		    i.setGLOptions('opaque')
 		
+		if self.blend_v == 4:  
+		    i.setGLOptions('costume')
+		    glBlendColor(self.blend_r,self.blend_g,self.blend_b,self.blend_a)
 		    
 		i.paint()
                   
@@ -184,6 +200,20 @@ class GLViewWidget(gl.GLViewWidget):
     def value_opaque(self,value):
 	self.blend_v = 0
 
+    def value_costume(self,value):
+	self.blend_v = 4
+	
+    def value_r(self,value):
+	self.blend_r = float(value)/100.0
+
+    def value_g(self,value):
+	self.blend_b = float(value)/100.0
+	
+    def value_b(self,value):
+	self.blend_g = float(value)/100.0
+
+    def value_a(self,value):
+	self.blend_a = float(value)/100.0
 	
 class Window(QtGui.QWidget):
     NumRows = 2
@@ -227,14 +257,59 @@ class Window(QtGui.QWidget):
 	self.button_additive = QtGui.QPushButton('additive', self)
         self.button_translucent = QtGui.QPushButton('translucent', self)
         self.button_opaque = QtGui.QPushButton('opaque', self)
+        self.button_costume = QtGui.QPushButton('costume', self)
         
         mainLayout.addWidget(self.button_additive,2,0)
 	mainLayout.addWidget(self.button_translucent,3,0)
         mainLayout.addWidget(self.button_opaque,4,0)
+        mainLayout.addWidget(self.button_costume,5,0)
         
         self.button_additive.clicked.connect(self.currentGlWidget.value_additive)
         self.button_translucent.clicked.connect(self.currentGlWidget.value_translucent)
         self.button_opaque.clicked.connect(self.currentGlWidget.value_opaque)
+        self.button_costume.clicked.connect(self.currentGlWidget.value_costume)
+        
+        ##Add sliders
+        self.slider_one = QtGui.QSlider(QtCore.Qt.Horizontal)
+	self.slider_one.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.slider_one.setTickPosition(QtGui.QSlider.TicksBothSides)
+        self.slider_one.setTickInterval(100)
+        self.slider_one.setSingleStep(1)
+        self.slider_one.setMinimum(1)
+        
+        self.slider_two = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.slider_two.setGeometry(QtCore.QRect(20, 40, 140, 160))
+        self.slider_two.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.slider_two.setTickPosition(QtGui.QSlider.TicksBothSides)
+        self.slider_two.setTickInterval(100)
+        self.slider_two.setSingleStep(1)
+        self.slider_two.setMinimum(1)
+        
+        self.slider_tree = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.slider_tree.setGeometry(QtCore.QRect(20, 40, 140, 160))
+        self.slider_tree.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.slider_tree.setTickPosition(QtGui.QSlider.TicksBothSides)
+        self.slider_tree.setTickInterval(100)
+        self.slider_tree.setSingleStep(1)
+        self.slider_tree.setMinimum(1)
+        
+        self.slider_four = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.slider_four.setGeometry(QtCore.QRect(20, 40, 140, 160))
+        self.slider_four.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.slider_four.setTickPosition(QtGui.QSlider.TicksBothSides)
+        self.slider_four.setTickInterval(100)
+        self.slider_four.setSingleStep(1)
+        self.slider_four.setMinimum(1)
+        
+        self.slider_one.valueChanged.connect(self.currentGlWidget.value_r)
+        self.slider_two.valueChanged.connect(self.currentGlWidget.value_g)
+        self.slider_tree.valueChanged.connect(self.currentGlWidget.value_b)
+        self.slider_four.valueChanged.connect(self.currentGlWidget.value_a)
+        mainLayout.addWidget(self.slider_one,2,2)
+        mainLayout.addWidget(self.slider_two,3,2)
+        mainLayout.addWidget(self.slider_tree,4,2)
+        mainLayout.addWidget(self.slider_four,5,2)
+        
         
         self.setLayout(mainLayout)
         self.setWindowTitle("Textures")
@@ -243,10 +318,21 @@ class Window(QtGui.QWidget):
 	self.button_additive.clicked.disconnect(self.currentGlWidget.value_additive)
         self.button_translucent.clicked.disconnect(self.currentGlWidget.value_translucent)
         self.button_opaque.clicked.disconnect(self.currentGlWidget.value_opaque)
+        self.button_costume.clicked.disconnect(self.currentGlWidget.value_costume)
+        self.slider_one.valueChanged.disconnect(self.currentGlWidget.value_r)
+        self.slider_two.valueChanged.disconnect(self.currentGlWidget.value_g)
+        self.slider_tree.valueChanged.disconnect(self.currentGlWidget.value_b)
+        self.slider_four.valueChanged.disconnect(self.currentGlWidget.value_a)
+        
         self.currentGlWidget = self.sender()
         self.button_additive.clicked.connect(self.currentGlWidget.value_additive)
         self.button_translucent.clicked.connect(self.currentGlWidget.value_translucent)
         self.button_opaque.clicked.connect(self.currentGlWidget.value_opaque)
+        self.button_costume.clicked.connect(self.currentGlWidget.value_costume)
+        self.slider_one.valueChanged.connect(self.currentGlWidget.value_r)
+        self.slider_two.valueChanged.connect(self.currentGlWidget.value_g)
+        self.slider_tree.valueChanged.connect(self.currentGlWidget.value_b)
+        self.slider_four.valueChanged.connect(self.currentGlWidget.value_a)
 
     def rotateOneStep(self):
         if self.currentGlWidget:
